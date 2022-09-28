c1792d295dc4d609e8f84cb304525cb95bf10dc4
valery-shvetsov <113056546+valery-shvetsov@users.noreply.github.com>
2022-09-28 20:55:28 +0300

    ex25

---
+++ ex25/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и 
+//возводит число A в натуральную степень B.
+int ReadData(string line)
+{
+    Console.WriteLine(line);
+    int number = int.Parse(Console.ReadLine() ?? "");
+    return number;
+}
+void PrintData(string prefix, string value) 
+{     
+    Console.WriteLine(prefix + value); 
+}
+
+double basis = ReadData("Введите основание степени : ");
+double index = ReadData("Введите показатель степени : ");
+double result = Math.Pow(basis,index);
+PrintData("Результат возведения в степень =",result.ToString());
+
+
+
