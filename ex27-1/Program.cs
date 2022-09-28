50fb4640d357e1ee9d1727eb98cffc773ad41b67
valery-shvetsov <113056546+valery-shvetsov@users.noreply.github.com>
2022-09-28 22:37:14 +0300

    ex27-1

---
+++ ex27-1/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+int ReadData(string line)
+{
+    Console.WriteLine(line);
+    int number = int.Parse(Console.ReadLine() ?? "");
+    return number;
+}
+void PrintData(string prefix, string value) 
+{     
+    Console.WriteLine(prefix + value);
+}
+
+//main
+int number = ReadData("Введите целое число: ");
+int summ=0; 
+while (number>=1)
+{
+    int a=number%10;
+//    Console.WriteLine(a);
+    summ=summ+a;
+    number=number/10;
+} 
+PrintData("Сумма цифр в числе: ", summ.ToString());  
